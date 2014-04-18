7e61d8e1631643f5a91258c9a2944ca8e37624ad
malibin <mlb0903@163.com>
2014-04-18 10:18:07 +0800

    helper.rb full_title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title(title_name)
+    basic_title = "Ruby on Rails Tutorial Sample App"
+    if title_name.blank?
+      return basic_title
+    else
+      return "#{basic_title} | #{title_name}"
+    end
+  end
 end
